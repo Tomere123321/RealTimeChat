b804ebe406b731c37738f9f43d96c0768d61fe01
Tomere123321 <tomere1111@hotmail.com>
2024-10-25 13:53:56 +0300

    changes

--- BackEnd/Controller/massageController.js
+++ BackEnd/Controller/massageController.js
@@ -5,69 +5,17 @@ const messageModel = require("../Model/messageModel");
 
 const router = express.Router();
 
-// const sendMessage = async (req, res) => {
-//   try {
-//     const { message } = req.body;
-//     const { id: receiverId } = req.params; 
-//     const senderId = req.user._id;
-
-//     console.log("Sender ID:", senderId);
-//     console.log("Receiver ID:", receiverId);
-//     console.log("Message:", message);
-
-   
-//     let conversation = await conversationModel.findOne({
-//       participants: { $all: [senderId, receiverId] },
-//     });
-
-//     if (!conversation) {
-//       conversation = await conversationModel.create({
-//         participants: [senderId, receiverId],
-//       });
-//     }
-
-   
-//     const newMessage =  new messageModel({
-//       senderId,
-//       receiverId,
-//       message,
-//   })
-
-//   await newMessage.save();
-//   console.log("New message saved:", newMessage);
-  
-//   // if (newMessage) {
-//   //   conversation.messages.push(newMessage._id);
-//   // }
-
-//   conversation.messages.push(newMessage._id);
-//   await conversation.save();
-    
-//     await conversation.save();
-//     await newMessage.save()
-   
-//     console.log("New message saved:", newMessage);
-
-//     conversation.messages.push(newMessage._id);
-//     await conversation.save();
-  
-//     res.status(201).json(newMessage);
-
-//   } catch (e) {
-//     console.log("error in Send Massage Controller:", e.message);
-//     res.status(500).json({ error: "internal Server Error" });
-//   }
-// };
-
-// router.post("/send/:id", protectRoute, sendMessage);
-
 const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
     const { id: receiverId } = req.params; 
     const senderId = req.user._id;
 
+    console.log("Sender ID:", senderId);
+    console.log("Receiver ID:", receiverId);
+    console.log("Message:", message);
 
+   
     let conversation = await conversationModel.findOne({
       participants: { $all: [senderId, receiverId] },
     });
@@ -78,29 +26,37 @@ const sendMessage = async (req, res) => {
       });
     }
 
-    const newMessage = new messageModel({
+   
+    const newMessage =  new messageModel({
       senderId,
       receiverId,
       message,
-    });
+  })
 
-    await newMessage.save();
-<<<<<<< HEAD
+  await newMessage.save();
+  console.log("New message saved:", newMessage);
   
-=======
-   
+  // if (newMessage) {
+  //   conversation.messages.push(newMessage._id);
+  // }
 
->>>>>>> parent of 5234e8e (Added socket)
+  conversation.messages.push(newMessage._id);
+  await conversation.save();
+    
+    await conversation.save();
+    await newMessage.save()
+   
+    console.log("New message saved:", newMessage);
+
     conversation.messages.push(newMessage._id);
     await conversation.save();
-
+  
     res.status(201).json(newMessage);
+
   } catch (e) {
-    console.log("error in Send Message Controller:", e.message);
+    console.log("error in Send Massage Controller:", e.message);
     res.status(500).json({ error: "internal Server Error" });
   }
 };
 
 router.post("/send/:id", protectRoute, sendMessage);
-
-module.exports = router;
